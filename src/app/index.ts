469d083bbba5abb8e084954651b0d5cb3e530dac
Bradley Lyman <lyman.brad3211@gmail.com>
2023-12-17 19:47:16 -0700

    fix: add padding + formatting for canvas

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,9 @@
 import { Page } from "./lib/page";
 
-import inscribed_square from "./sketches/inscribed_square";
-import text from "./sketches/text";
+import hello_color from "./sketches/hello_color";
+import hello_shape from "./sketches/hello_shape";
 
-const page = new Page("Magic Circles");
-page.InstallSketches([inscribed_square, text]);
+const page = new Page("P5 Gallery Starter");
+page.InstallSketches([hello_color, hello_shape]);
 
 export {};
