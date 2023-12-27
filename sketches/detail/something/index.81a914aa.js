a31e4e6fdd39b708c81b96dddffe6ded0119a304
BradleyLyman <BradleyLyman@users.noreply.github.com>
2023-12-27 16:41:03 +0000

    deploy: fc8fa73295f683d6f2c84e36bb3bba1164a7435e

--- sketches/detail/something/index.81a914aa.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello from index.ts");
-//# sourceMappingURL=index.81a914aa.js.map
--- sketches/scratch/hello/index.f112ee03.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello World From Stuff.ts");
-//# sourceMappingURL=index.f112ee03.js.map
