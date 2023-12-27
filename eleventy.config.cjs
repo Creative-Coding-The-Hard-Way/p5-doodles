f07188d69c38af4b82ff12b11b890e725023ae11
Bradley Lyman <lyman.brad3211@gmail.com>
2023-12-26 21:14:39 -0700

    setup index page and automatic sketch detection

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,6 +1,23 @@
 module.exports = function (config) {
-  config.addPassthroughCopy("site/manifest/*");
-  config.addPassthroughCopy("site/**/*.css");
+  config.addCollection("categories", (collectionApi) => {
+    const sketches = collectionApi.getAll().filter((entry) => {
+      return entry.data.sketch != undefined;
+    });
+    const categories = sketches.map((sketch) => {
+      return sketch.data.sketch.category;
+    });
+    const unique = [...new Set(categories)];
+    console.log("Found categories: ", unique);
+    return unique;
+  });
+
+  config.addCollection("sketches", (collectionApi) => {
+    const sketches = collectionApi.getAll().filter((entry) => {
+      return entry.data.sketch != undefined;
+    });
+    console.log("Add", sketches.length, "sketches to the sketches collection");
+    return sketches;
+  });
 
   return {
     dir: {
--- site/_data/sketches.js
+++ site/_data/sketches.js
@@ -3,17 +3,64 @@ const path = require("path");
 
 const sketches_path = path.join("src", "sketches");
 
-module.exports = async () => {
-  const files = fs.readdirSync(path.join(__dirname, "../../", sketches_path));
+/**
+ * Construct a filter predicate which removes all path elements
+ * which are not directories.
+ *
+ * Paths are constructed relative to a provided root path.
+ */
+function is_directory(root) {
+  return (file) => {
+    const full_path = path.join(root, file);
+    const stat = fs.lstatSync(full_path);
+    if (stat.isDirectory()) {
+      return true;
+    } else {
+      console.warn(`skipping {file} because it is not a directory`);
+      return false;
+    }
+  };
+}
 
-  const sketches = files.map((file) => {
-    return {
-      name: path.parse(file).name,
-      path: path.join("~", sketches_path, file),
-    };
+module.exports = async () => {
+  const sketches_root = path.join(__dirname, "../../", sketches_path);
+
+  /**
+   * Categories are all of the root-level folders in the sketches directory.
+   */
+  const categories = fs
+    .readdirSync(sketches_root)
+    .filter(is_directory(sketches_root));
+
+  /**
+   * Each category can have 0 or more sketches.
+   */
+  const sketches = categories.flatMap((category_folder) => {
+    const category_path = path.join(sketches_root, category_folder);
+    const sketch_folders = fs
+      .readdirSync(category_path)
+      .filter(is_directory(category_path));
+
+    const sketches_in_category = sketch_folders.map((sketch_folder) => {
+      const sketch_path = path.join(category_path, sketch_folder);
+      const sketch_files = fs.readdirSync(sketch_path);
+      const source = sketch_files.find((file) => path.extname(file) == ".ts");
+      return {
+        name: sketch_folder,
+        category: category_folder,
+        source: path.join(
+          "~",
+          sketches_path,
+          category_folder,
+          sketch_folder,
+          source
+        ),
+      };
+    });
+    return sketches_in_category;
   });
 
-  console.log("FOUND sketches ", JSON.stringify(sketches, undefined, 2));
+  console.log("found sketches ", JSON.stringify(sketches, undefined, 2));
 
   return sketches;
 };
